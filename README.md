9926b9f4848fc04f194395ef98dd7a9c40cf4d16
solygambas <solygambas@gmail.com>
2020-05-12 12:22:48 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Blogging Platform
+
+A blogging platform using React.
+
+Based on [React For The Rest Of Us](https://www.udemy.com/course/react-for-the-rest-of-us/) by Brad Schiff (2020).
--- app/Main.js
+++ app/Main.js
@@ -3,10 +3,126 @@ import ReactDOM from "react-dom";
 
 function ExampleComponent() {
   return (
-    <div>
-      <h1>This is our app!</h1>
-      <p>The sky is blue and the grass is green.</p>
-    </div>
+    <>
+      <header className="header-bar bg-primary mb-3">
+        <div className="container d-flex flex-column flex-md-row align-items-center p-3">
+          <h4 className="my-0 mr-md-auto font-weight-normal">
+            <a href="/" className="text-white">
+              ComplexApp
+            </a>
+          </h4>
+          <form className="mb-0 pt-2 pt-md-0">
+            <div className="row align-items-center">
+              <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
+                <input
+                  name="username"
+                  className="form-control form-control-sm input-dark"
+                  type="text"
+                  placeholder="Username"
+                  autocomplete="off"
+                />
+              </div>
+              <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
+                <input
+                  name="password"
+                  className="form-control form-control-sm input-dark"
+                  type="password"
+                  placeholder="Password"
+                />
+              </div>
+              <div className="col-md-auto">
+                <button className="btn btn-success btn-sm">Sign In</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </header>
+
+      <div className="container py-md-5">
+        <div className="row align-items-center">
+          <div className="col-lg-7 py-3 py-md-5">
+            <h1 className="display-3">Remember Writing?</h1>
+            <p className="lead text-muted">
+              Are you sick of short tweets and impersonal &ldquo;shared&rdquo;
+              posts that are reminiscent of the late 90&rsquo;s email forwards?
+              We believe getting back to actually writing is the key to enjoying
+              the internet again.
+            </p>
+          </div>
+          <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
+            <form>
+              <div className="form-group">
+                <label for="username-register" className="text-muted mb-1">
+                  <small>Username</small>
+                </label>
+                <input
+                  id="username-register"
+                  name="username"
+                  className="form-control"
+                  type="text"
+                  placeholder="Pick a username"
+                  autocomplete="off"
+                />
+              </div>
+              <div className="form-group">
+                <label for="email-register" className="text-muted mb-1">
+                  <small>Email</small>
+                </label>
+                <input
+                  id="email-register"
+                  name="email"
+                  className="form-control"
+                  type="text"
+                  placeholder="you@example.com"
+                  autocomplete="off"
+                />
+              </div>
+              <div className="form-group">
+                <label for="password-register" className="text-muted mb-1">
+                  <small>Password</small>
+                </label>
+                <input
+                  id="password-register"
+                  name="password"
+                  className="form-control"
+                  type="password"
+                  placeholder="Create a password"
+                />
+              </div>
+              <button
+                type="submit"
+                className="py-3 mt-4 btn btn-lg btn-success btn-block"
+              >
+                Sign up for ComplexApp
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      <footer className="border-top text-center small text-muted py-3">
+        <p>
+          <a href="/" className="mx-1">
+            Home
+          </a>{" "}
+          |{" "}
+          <a className="mx-1" href="/about-us">
+            About Us
+          </a>{" "}
+          |{" "}
+          <a className="mx-1" href="/terms">
+            Terms
+          </a>
+        </p>
+        <p className="m-0">
+          Copyright &copy; 2020{" "}
+          <a href="/" className="text-muted">
+            ComplexApp
+          </a>
+          . All rights reserved.
+        </p>
+      </footer>
+    </>
   );
 }
 
