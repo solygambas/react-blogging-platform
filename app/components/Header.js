c25e6fada8a8f166159a03d4c7bfebbd35ca7d21
solygambas <solygambas@gmail.com>
2020-05-13 09:53:54 +0200

    add axios requests

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import HeaderLoggedOut from "./HeaderLoggedOut";
 
 function Header() {
   return (
@@ -10,30 +11,7 @@ function Header() {
             Janis
           </Link>
         </h4>
-        <form className="mb-0 pt-2 pt-md-0">
-          <div className="row align-items-center">
-            <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-              <input
-                name="username"
-                className="form-control form-control-sm input-dark"
-                type="text"
-                placeholder="Username"
-                autoComplete="off"
-              />
-            </div>
-            <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-              <input
-                name="password"
-                className="form-control form-control-sm input-dark"
-                type="password"
-                placeholder="Password"
-              />
-            </div>
-            <div className="col-md-auto">
-              <button className="btn btn-success btn-sm">Sign In</button>
-            </div>
-          </div>
-        </form>
+        <HeaderLoggedOut />
       </div>
     </header>
   );
--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import Axios from "axios";
 import Page from "./Page";
 
 function HomeGuest() {
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      await Axios.post("http://localhost:8080/register", {
+        username,
+        email,
+        password,
+      });
+      console.log("User was successfully created.");
+    } catch (e) {
+      console.log("There was an error.");
+    }
+  }
   return (
     <Page title="Welcome!" wide={true}>
       <div className="row align-items-center">
@@ -15,12 +32,13 @@ function HomeGuest() {
           </p>
         </div>
         <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-group">
               <label htmlFor="username-register" className="text-muted mb-1">
                 <small>Username</small>
               </label>
               <input
+                onChange={(e) => setUsername(e.target.value)}
                 id="username-register"
                 name="username"
                 className="form-control"
@@ -34,6 +52,7 @@ function HomeGuest() {
                 <small>Email</small>
               </label>
               <input
+                onChange={(e) => setEmail(e.target.value)}
                 id="email-register"
                 name="email"
                 className="form-control"
@@ -47,6 +66,7 @@ function HomeGuest() {
                 <small>Password</small>
               </label>
               <input
+                onChange={(e) => setPassword(e.target.value)}
                 id="password-register"
                 name="password"
                 className="form-control"
